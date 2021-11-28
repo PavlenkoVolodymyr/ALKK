84768e032fee1f0defa2579449bc6f702a143e32
PavlenkoVolodymyr <volodymyrp88@gmail.com>
2021-11-28 15:21:14 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import TopNavigation from './components/TopNavigation/TopNavigation';
-import './App.css';
 import 'normalize.css';
+import './App.css';
+
+import BottomNavigation from './components/BottomNavigation/BottomNavigation';
+import TopNavigation from './components/TopNavigation/TopNavigation';
 
 function App() {
-  return <TopNavigation />;
+  return (
+    <>
+      <TopNavigation />
+      <BottomNavigation />
+    </>
+  );
 }
 
 export default App;
